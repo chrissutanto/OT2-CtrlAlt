f6b0e1a167c5c909a77134c6bdb9c471a79922dc
chrissutanto <chris.sutanto28@gmail.com>
2020-06-16 13:04:40 -0700

    Changed structure of GetDrive

--- ControlApp.py
+++ ControlApp.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+from GetDrive import getProtocol
+
 app = Flask(__name__)
+
 
 def sampleText():
     return "hi"
 
 @app.route('/')
 def home():
+    getProtocol()
     return render_template('home.html')
 
 if __name__== '__main__':
