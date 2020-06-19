10fbb07212c26a7fde94c25f4d84ba00d6e1d44e
chrissutanto <chris.sutanto28@gmail.com>
2020-06-19 11:06:19 -0700

    Now sorts labware list by deck location

--- ScriptHandler.py
+++ ScriptHandler.py
@@ -1,5 +1,7 @@
 import re
 
+# Protocol must contain item type and location in same line, location must be defined in the load statement
+
 # Takes protcol ID, returns lines of script
 def getLines(protocol_id):
     file = open('protocol_files/{}.py'.format(protocol_id))
@@ -14,7 +16,11 @@ def getItem(line):
     location = re.search(pattern, afterComma)[1]
     return {'location': location, 'item': item}
 
-# Takes protocol ID, returns dictionary containing labware info
+# Takes labware, returns deck location, used to sort list of labware in order of deck location
+def getLocation(labware):
+    return labware['location']
+
+# Takes protocol ID, returns list of dict containing labware info
 def findLabware(protocol_id):
     labware = []
     lines = getLines(protocol_id)
@@ -25,9 +31,10 @@ def findLabware(protocol_id):
             elif "'" in lines[i+1]:
                 new_item = getItem(lines[i+1])
             labware.append(new_item)
+    labware.sort(key=getLocation)
     return labware
 
-# Takes protocol ID, returns dictionary containing pipette info
+# Takes protocol ID, returns list of dict containing pipette info
 def findPipettes(protocol_id):
     pipettes = []
     lines = getLines(protocol_id)
