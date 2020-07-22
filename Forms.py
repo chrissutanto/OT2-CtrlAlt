2ba593950a0810023a4672207ebab98c7ea90ae4
chrissutanto <chris.sutanto28@gmail.com>
2020-07-22 16:12:53 -0700

    Added additional labware options in dropdown

--- Forms.py
+++ Forms.py
@@ -7,7 +7,9 @@ class fieldForm(FlaskForm):
 
 class LabwareForm(FlaskForm):
     choices = [('opentrons_10_tuberack_falcon_4x50ml_6x15ml_conical', '10 Tube Rack with Falcon 4x50mL, 6x15mL Conical'),
-                ('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '24 Tube Rack with Eppendorf 1.5 mL Safe-Lock Snapcap')]
+                ('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '24 Tube Rack with Eppendorf 1.5mL Safe-Lock Snapcap'),
+                ('opentrons_15_tuberack_falcon_15ml_conical', '15 Tube Rack with Falcon 15mL Conical'),
+                ('opentrons_6_tuberack_falcon_50ml_conical', '6 Tube Rack with Falcon 50mL conical')]
     source = SelectField(u'Source', choices=choices)
     destination = SelectField(u'Destination', choices=choices)
 
